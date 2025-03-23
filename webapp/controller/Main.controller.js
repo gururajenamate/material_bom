0414885769e989f5dd574a259e2bd01a5cf1a931
Ambika Enamate <ambika.enamate@gmail.com>
2025-03-23 13:24:16 +0000

    Dynamic Column Data

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -8,61 +8,86 @@ sap.ui.define([
 
     return Controller.extend("com.ulli.materialbom.controller.Main", {
         onInit() {
+            this.initialLoad = true;
         },
 
-        toggleOpenState: function(oEvent) {
-            debugger;
+        toggleOpenState: function (oEvent) {
             this.expand = oEvent.getParameter("expanded");
-            if(this.expand) {
+            if (this.expand) {
                 var oSelectedObj = oEvent.getParameter("rowContext").getObject();
 
-                if(oSelectedObj.HierarchyLevel === 0) {
+                if (oSelectedObj.HierarchyLevel === 0) {
                     this.ParentNodeID = oSelectedObj.NodeID;
                 }
             }
         },
 
-        rowsUpdated: function(oEvent) {
-            if(this.expand) {
-                var oTable = oEvent.getSource();
-                var aRows = oTable.getRows();
+        rowsUpdated: function (oEvent) {
+            var oTable = oEvent.getSource();
+            var aRows = oTable.getRows();
+            if (this.expand) {
                 var oParentNodeID = this.ParentNodeID;
-                
-                aRows.forEach(function(oRow) {
+
+                aRows.forEach(function (oRow) {
                     var oContext = oRow.getBindingContext();
                     var oData = oContext?.getObject();
-                    
+
                     if (oData && oData.ParentNodeID === oParentNodeID) {
+
                         this.expandTree(oData.NodeID);
-                        
+
                         if (oRow.isExpandable() && !oRow.isExpanded()) {
+                            this.fetchColumnData(oContext);
                             oTable.expand(oRow.getIndex());
+                        } else if (!oRow.isExpandable()) {
+                            this.fetchColumnData(oContext);
                         }
                     }
+                }, this);
+            } else if(this.initialLoad){
+                this.initialLoad = false;
+                aRows.forEach(function (oRow) {
+                    var oContext = oRow.getBindingContext();
+                    this.fetchColumnData(oContext);
                 }, this);
             }
         },
 
-        expandTree: function(oParentNodeID) {
+        expandTree: function (oParentNodeID) {
             var oTable = this.getView().byId("treeTable");
             var aRows = oTable.getRows();
             // var oParentNodeID = this.ParentNodeID;
-            
-            aRows.forEach(function(oRow) {
+
+            aRows.forEach(function (oRow) {
                 var oContext = oRow.getBindingContext();
                 var oData = oContext?.getObject();
-                
+
                 if (oData && oData.ParentNodeID === oParentNodeID) {
+
                     this.expandTree(oData.NodeID);
-                    
+
                     if (oRow.isExpandable() && !oRow.isExpanded()) {
+                        this.fetchColumnData(oContext);
                         oTable.expand(oRow.getIndex());
+                    } else if (!oRow.isExpandable()) {
+                        this.fetchColumnData(oContext);
                     }
                 }
             }, this);
+        },
+
+        fetchColumnData: function (oContext) {
+            // Call service here and set data for columns
+            if (oContext && !oContext.getProperty("Column1")) {
+                const m = oContext.getProperty("Description")+"_";
+                oContext.setProperty("Column1", m+Math.random().toString(36).substring(7));
+                oContext.setProperty("Column2", m+Math.random().toString(36).substring(7));
+                oContext.setProperty("Column3", m+Math.random().toString(36).substring(7));
+                oContext.setProperty("Column4", m+Math.random().toString(36).substring(7));
+                oContext.setProperty("Column5", m+Math.random().toString(36).substring(7));
+            }
         }
 
 
-        
     });
 });
